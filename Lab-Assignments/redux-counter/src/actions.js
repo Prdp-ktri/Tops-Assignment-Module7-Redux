c338727ab57004d129b91df89c2bd829c1415ed2
Pradeep Khatri <pradeepkhatri44@live.com>
2025-10-14 16:55:39 +0530

    rmeoving
    rmeoving

--- Lab-Assignments/redux-counter/src/actions.js
+++
@@ -1,9 +0,0 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-
-export const incrementcounter = () => ({
-  type: INCREMENT,
-});
-export const decrementCounter = () => ({
-  type: DECREMENT,
-})
--- Lab-Assignments/redux-counter/src/store.js
+++
@@ -1,8 +0,0 @@
-import counterReducer from "./reducer";
-import { configureStore } from "@reduxjs/toolkit";
-
-const store = configureStore({
-  reducer: counterReducer,
-});
-
-export default store;
